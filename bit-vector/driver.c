080ba6b305e9414469a33dfe51e3c5b035832670
Hansel <valkirecesa@gmail.com>
2021-10-08 00:14:56 +0800

    finish bit vector

--- bit-vector/driver.c
+++ bit-vector/driver.c
@@ -2,15 +2,17 @@
 #define SIZE 14
 typedef int SET[SIZE];
 
+
+void initSet(SET S)
+{
+    for (int i = 0; i < SIZE; i++)
+        S[i] = 0;
+}
+
 void displaySet(SET S)
 {
-    for (int i = 0; i < SIZE; i++) {
-        // Need to explicitly compare to 1
-        // because I did initialize SET to 0
-        // SET may contain 1 as its garbage value :)
-        if (S[i] == 1)
-            printf("%d ", i);
-    }
+    for (int i = 0; i < SIZE; i++)
+        if (S[i]) printf("%d ", i);
     printf("\n");
 }
 
@@ -24,6 +26,39 @@ void populate(SET S, int data[], int size)
     for (int i = 0; i < size; i++)
         insertToSet(S, data[i]);
 }
+
+// union = all elements in SET A and SET B
+SET *unionSet(SET A, SET B)
+{
+    SET *C = (SET *) calloc (SIZE, sizeof(SET));
+
+    for (int i = 0; i < SIZE; i++)
+        (*C)[i] = A[i] | B[i];
+
+    return C;
+}
+
+// intersection = all elements of A that also belong to B
+SET *intersectionSet(SET A, SET B)
+{
+    SET *C = (SET *) calloc (SIZE, sizeof(SET));
+
+    for (int i = 0; i < SIZE; i++)
+        (*C)[i] = A[i] & B[i];
+    
+    return C;
+}
+
+// set difference = elements in A but not in B
+SET *setDifference(SET A, SET B)
+{
+    SET *C = (SET *) calloc (SIZE, sizeof(SET));
+
+    for (int i = 0; i < SIZE; i++)
+        (*C)[i] = A[i] & ~B[i];
+    return C;
+}
+
 
 int main()
 {
@@ -43,6 +78,7 @@ int main()
 
     int data[] = {0, 2, 3, 5, 7, 8, 11, 12, 13};
     SET B;
+    initSet(B);
     populate(B, data, 9);
     /**
      *
@@ -55,8 +91,55 @@ int main()
     **/ 
     printf("SET B: ");
     displaySet(B);
+
     
 
+    SET *C;
+    C = unionSet(A, B);
+    printf("\nPerforming A union B...\n");
+    printf("SET C: ");
+    displaySet(C);
+    /**
+     *
+     *  +---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+     *  | 1 | 0 | 1 | 1 | 0 | 1 | 1 | 1 | 1 | 1 | 0 | 1 | 1 | 1 |
+     *  +---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+     *    0   1   2   3   4   5   6   7   8   9  10  11  12   13
+     * 
+     * 
+    **/ 
+
+
+    SET *D;
+    D = intersectionSet(A, B);
+    printf("\nPerforming A intersection B...\n");
+    printf("SET D: ");
+    displaySet(D);
+    /**
+     *
+     *  +---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+     *  | 1 | 0 | 0 | 1 | 0 | 0 | 0 | 0 | 1 | 0 | 0 | 1 | 0 | 1 |
+     *  +---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+     *    0   1   2   3   4   5   6   7   8   9  10  11  12   13
+     * 
+     * 
+    **/ 
+
+
+    SET *E;
+    E = setDifference(A, B);
+    printf("\nPerforming A difference B...\n");
+    printf("SET E: ");
+    displaySet(E);
+    /**
+     *
+     *  +---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+     *  | 0 | 0 | 0 | 0 | 0 | 0 | 1 | 0 | 0 | 1 | 0 | 0 | 0 | 0 |
+     *  +---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+     *    0   1   2   3   4   5   6   7   8   9  10  11  12   13
+     * 
+     * 
+    **/ 
 
 
     return 0;
